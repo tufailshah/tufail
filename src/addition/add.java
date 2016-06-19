724a7da110a32702e29795a08a271a26e1415444
Tufail Shah Hashmi <Tufail Shah Hashmi@192.168.10.63>
2016-06-20 01:25:17 +0500

    its kkk

--- src/addition/add.java
+++ src/addition/add.java
@@ -1,6 +1,12 @@
 package addition;
 
 public class add {
+	/**
+	 * 
+	 * @param args
+	 * <h1>this is the first tag</h1>
+	 * <p>this is first paragraph</p>
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,6 +20,8 @@ int y=0;
 int l=2;
 int kl = 10;
 int tu=110;
+int ali=110;
+int kkk=0;
 	}
 
 }
