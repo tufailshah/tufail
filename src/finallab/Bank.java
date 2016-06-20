9f016d18ca64a8b15af0bce7db8275858a376cb9
Tufail Shah Hashmi <Tufail Shah Hashmi@172.16.105.110>
2016-06-20 14:23:35 +0500

    its final

---
+++ src/finallab/Bank.java
@@ -0,0 +1,46 @@
+package finallab;
+
+public class Bank extends Account {
+public void addAccount()
+{
+	int g=0;
+	int a=0;
+	}
+
+public int selectAccount(int s)
+
+{
+	s=3;
+	return s;
+	}
+
+@Override
+public void get_number(int num) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void get_balance(int bal) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void get_owner() {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void get_deposit() {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void withdraw() {
+	// TODO Auto-generated method stub
+	
+}
+}
